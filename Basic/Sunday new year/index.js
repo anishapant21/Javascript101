8515ef2a834592af2bfb75b4c05c14106c69b5a5
eternal-moon <anishapant1@gmail.com>
2021-05-15 09:25:14 +0545

    files added

---
+++ Basic/Sunday new year/index.js
@@ -0,0 +1,14 @@
+function findFun(){
+    for ( var year=2014 ; year <=2050; year++){
+        d = new Date(year, 0, 1);
+        yearHere=[];
+        if (d.getDay()== 0){
+            console.log("Sunday on the new year day is on year", year);
+           
+            
+        }
+    }
+    
+}
+
+findFun();
